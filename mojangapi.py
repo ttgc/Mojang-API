331a0efaded5ab55713022160d89a9c886793534
ttgc <ttgc@users.noreply.github.com>
2017-04-12 15:06:05 +0200

    Add files via upload

--- mojangapi.py
+++ mojangapi.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib.request as urllib2
 import json
 
 
@@ -7,14 +7,14 @@ class MojangAPI:
     def _get_json(self, url):
         req = urllib2.Request(
             url=url, headers={'Content-Type': 'application/json'})
-        response = json.loads(urllib2.urlopen(req).read())
+        response = json.loads(urllib2.urlopen(req).read().decode())
 
         return response
 
     def _post_json(self, url, data):
         req = urllib2.Request(
-            url=url, headers={'Content-Type': 'application/json'}, data=json.dumps(data))
-        response = json.loads(urllib2.urlopen(req).read())
+            url=url, headers={'Content-Type': 'application/json'}, data=json.dumps(data).encode())
+        response = json.loads(urllib2.urlopen(req).read().decode())
 
         return response
 
@@ -25,23 +25,24 @@ class MojangAPI:
             url = url + '?service=' + service
 
         statuses = self._get_json(url)
+        return statuses
 
-    def mojang_news(self):
-        url = 'http://status.mojang.com/news'
-
-        return self._get_json(url)
+##    def mojang_news(self):
+##        url = 'http://status.mojang.com/news'
+##
+##        return self._get_json(url)
 
     def get_uuid(self, username):
-        url = 'https://api.mojang.com/profiles/page/1'
+        url = 'https://api.mojang.com/profiles/minecraft'#/page/1'
 
-        data = {
-            'name': username,
-            'agent': 'minecraft'
-        }
+        data = [#{
+            username#'name': username,
+            #'agent': 'minecraft'
+        ]#}
 
         uuid = self._post_json(url, data)
 
-        if uuid['size'] is not 1:
+        if len(uuid) is not 1:
             return None
 
-        return uuid['profiles'][0]
+        return uuid[0]['id']#['profiles'][0]
